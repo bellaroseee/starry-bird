111eb95fe40505cd377c114ca5d7be029828a078
Ronald Widjaja <ronaldwilliamwidjaja@gmail.com>
2021-07-10 11:39:13 -0700

    Fixed slow scroll due to hi-res background picture in Home page and fixed mobile home page not showing up.

--- site/src/Home.jsx
+++ site/src/Home.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 import birdIcon from "./assets/icons/bird.png";
 import nstIntroImg from "./assets/images/starry_night.jpg";
 import vgg19 from "./assets/images/vgg19.png";
-import demoImg from "./assets/images/wheatfield.jpg";
+import demoImg from "./assets/images/waves.jpg";
 
 function Home() {
 
@@ -42,8 +42,8 @@ function Home() {
                     </ScrollLink>
                 </Row>
             </Container>
-            <Fade delay={200}>
-                <Container fluid>
+            <Container fluid>
+                <Fade delay={100}>
                     <hr style={{borderWidth: "5px", marginTop: "10em", paddingBottom: "0px", marginBottom:"0px"}}/>
                     <Element name="nst">
                         <Row>
@@ -62,7 +62,7 @@ function Home() {
                                             }}>
                                                 <h1 className="text-left" style={{fontFamily: "'Fredoka One', cursive"}}>What's Neural Style Transfer?</h1>
                                                 <h5 style={{fontSize: "1.5em", fontFamily: "'Kalam', cursive"}}> 'Good artists never die, they just fade away...'</h5>
-                                                <p style={{fontSize: "1em", fontFamily: "'Karla', sans-serif"}}> 
+                                                <p style={{fontSize: "1em", fontFamily: "'Karla', sans-serif"}}>
                                                     Neural Style Transfer (NST) is a deep learning algorithm that combines the style of an image with the content of an image.
                                                     Think of it as re-painting a (content) image in the style of that artist, say, Vincent Van Gogh.
                                                 </p>
@@ -77,40 +77,41 @@ function Home() {
                             </Container>
                         </Row>
                     </Element>
-
-                    <hr style={{borderWidth: "5px", padding: "0px", margin:"0px"}}/>
-                    <Element name="methodology">
-                        <Row className="justify-content-center" style={{paddingTop: "15em", paddingBottom: "10em"}}>
-                            <Col style={{paddingLeft: "3em", paddingRight: "3em"}}>
-                                <Container>
-                                    <h1 style={{fontFamily: "'Fredoka One', cursive"}}>General Workflow</h1>
-                                    <p style={{fontSize: "1em", fontFamily: "'Karla', sans-serif"}}>
-                                        We utilized VGG-19 network layers to extract the features from the content and style image. 
-                                        <br/><br/>
-                                        For each train iteration, style content loss is calculated with gram matrix and gradient is applied to the image. 
-                                        <br/><br/>
-                                        A significant improvement we made was to expanded further by combining mulitple sytle images and then combine it with the single content image.
-                                    </p>
-                                    <Link to={'./about'}>
-                                        <Button variant="outline-dark">
-                                            Learn More
-                                        </Button>
-                                    </Link>
-                                </Container>
-                            </Col>
-                            <Col style={{paddingTop: "5em"}}>
-                                <Image src={vgg19} fluid/>
-                                <p style={{fontSize: "1.5vh", fontFamily: "'Karla', sans-serif"}} className="text-center">
-                                    (Fig 1.) The general workflow and pipeline for applying the Neural Style Transfer using the VGG-19 CNN.
-                                    <br/>
-                                    The style image (bottom-left) is applied to the base image (top-left) to produce the output (right).
+                </Fade>
+                <hr style={{borderWidth: "5px", padding: "0px", margin:"0px"}}/>
+                <Element name="methodology">
+                    <Row className="justify-content-center" style={{paddingTop: "15em", paddingBottom: "10em"}}>
+                        <Col style={{paddingLeft: "3em", paddingRight: "3em"}}>
+                            <Container>
+                                <h1 style={{fontFamily: "'Fredoka One', cursive"}}>General Workflow</h1>
+                                <p style={{fontSize: "1em", fontFamily: "'Karla', sans-serif"}}>
+                                    We utilized VGG-19 network layers to extract the features from the content and style image.
+                                    <br/><br/>
+                                    For each train iteration, style content loss is calculated with gram matrix and gradient is applied to the image.
+                                    <br/><br/>
+                                    A significant improvement we made was to expanded further by combining multiple style images and then combine it with the single content image.
                                 </p>
-                            </Col>
-                        </Row>
-                    </Element>
+                                <Link to={'./about'}>
+                                    <Button variant="outline-dark">
+                                        Learn More
+                                    </Button>
+                                </Link>
+                            </Container>
+                        </Col>
+                        <Col style={{paddingTop: "5em"}}>
+                            <Image src={vgg19} fluid/>
+                            <p style={{fontSize: "1.5vh", fontFamily: "'Karla', sans-serif"}} className="text-center">
+                                (Fig 1.) The general workflow and pipeline for applying the Neural Style Transfer using the VGG-19 CNN.
+                                <br/>
+                                The style image (bottom-left) is applied to the base image (top-left) to produce the output (right).
+                            </p>
+                        </Col>
+                    </Row>
+                </Element>
 
-                    <hr style={{borderWidth: "5px", padding: "0px", margin:"0px"}}/>
+                <hr style={{borderWidth: "5px", padding: "0px", margin:"0px"}}/>
 
+                <Fade delay={100}>
                     <Row>
                         <Container style={{
                             backgroundImage: `url(${demoImg})`,
@@ -139,8 +140,8 @@ function Home() {
                             </Element>
                         </Container>
                     </Row>
-                </Container>
-            </Fade>
+                </Fade>
+            </Container>
         </>
     );
 }
